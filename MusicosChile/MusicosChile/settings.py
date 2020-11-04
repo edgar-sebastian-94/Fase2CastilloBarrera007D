25d16c0eac4bae1f25fb4b721d40380d51011a77
Edgar Barrera Provoste <edgar94_sebastian@hotmail.com>
2020-11-04 01:57:24 -0300

    Creando pagina de inicio

--- MusicosChile/MusicosChile/settings.py
+++ MusicosChile/MusicosChile/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'index',
+    'index.apps.IndexConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MusicosChile.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [' ./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- MusicosChile/index/views.py
+++ MusicosChile/index/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from . models import Mensaje, Musico
+from django.views import generic
 
 # Create your views here.
+def index(request)
+    num_Mensaje = Mensaje.objects.all().count()
+    num_Musico = Musico.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_mensaje': num_Mensaje, 'num_musico': num_Musico},
+    )
