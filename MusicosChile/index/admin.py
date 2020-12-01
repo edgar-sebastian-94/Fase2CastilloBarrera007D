59ec1fe33fd496d626f561d1b4836b488ef127ec
Edgar Barrera Provoste <edgar94_sebastian@hotmail.com>
2020-11-30 23:22:46 -0300

    Entrega fase3 final4

--- MusicosChile/index/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from . models import Mensaje, Musico
-
-admin.site.register(Mensaje)
-admin.site.register(Musico)
--- MusicosChile/sesion/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SesionConfig(AppConfig):
-    name = 'sesion'
